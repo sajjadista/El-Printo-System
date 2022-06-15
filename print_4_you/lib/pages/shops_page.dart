5bed1a4940e29f0526c30a91098cf02bc1031ee4
AouIssa <51041563+AouIssa@users.noreply.github.com>
2022-06-15 16:07:48 +0800

    Updated Shops page

--- print_4_you/lib/pages/shops_page.dart
+++ print_4_you/lib/pages/shops_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -18,19 +19,110 @@ class ShopsPage extends StatelessWidget {
         point: LatLng(2.9993,101.7079),
         builder: (ctx) =>
             InkWell(
-              onTap: () {},
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    backgroundColor: Colors.red,
+                    title:
+                    Text('DarnLukis'
+                      ,style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.white,
+                      ),
+                    ),
+                    content: Text(
+                      'Price from this shop: 1RM \nDistance from your location: 1.4 km'
+                      ,style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
+                    ),
+
+                    actions: [
+                      InkWell(onTap: () => {},
+                        child: new Container(
+                          width: MediaQuery.of(context).size.width/3,
+                          height: MediaQuery.of(context).size.height/8,
+                          decoration: new BoxDecoration(
+                            color: Colors.white,
+                            border: new Border.all(color: Colors.white, width: 2.0),
+                            borderRadius: new BorderRadius.circular(10.0),
+                          ),
+                          child: new Center(child:
+                          new Text('Order Now',
+                            style: new TextStyle(
+                                fontSize: 18.0,
+                                color: Colors.red,
+                                fontWeight:
+                                FontWeight.w700),
+                          ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
               child: Image.asset(
                 'shop-icon.png',
                 width: 22,
                 fit: BoxFit.cover,
               ),
-            ),),
+            ),
+      ),
       //Shop2 location
       Marker(
         point: LatLng(2.992829362,101.704663848),
         builder: (ctx) =>
             InkWell(
-              onTap: () {},
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    backgroundColor: Colors.red,
+                    title: Text('Print4You'
+                      ,style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.white,
+                      ),
+                    ),
+                    content: Text(
+                      'Price from this shop: 2RM \nDistance from your location: 4.4 km'
+                      ,style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
+                    ),
+                    actions: [
+                      InkWell(onTap: () => {},
+                        child: new Container(
+                          width: MediaQuery.of(context).size.width/3,
+                          height: MediaQuery.of(context).size.height/8,
+                          decoration: new BoxDecoration(
+                            color: Colors.white,
+                            border: new Border.all(color: Colors.white, width: 2.0),
+                            borderRadius: new BorderRadius.circular(10.0),
+                          ),
+                          child: new Center(child:
+                          new Text('Order Now',
+                            style: new TextStyle(
+                                fontSize: 18.0,
+                                color: Colors.red,
+                                fontWeight:
+                                FontWeight.w700),
+                          ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
               child: Image.asset(
                 'shop-icon.png',
                 width: 22,
@@ -43,45 +135,89 @@ class ShopsPage extends StatelessWidget {
         point: LatLng(2.996829362,101.7033),
         builder: (ctx) =>
             InkWell(
-              onTap: () {},
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    backgroundColor: Colors.red,
+                    title: Text('Print Expert'
+                      ,style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.white,
+                      ),
+                    ),
+                    content: Text(
+                      'Price from this shop: 6RM \nDistance from your location: 2.4 km'
+                      ,style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
+                    ),
+                    actions: [
+                      InkWell(onTap: () => {},
+                        child: new Container(
+                          width: MediaQuery.of(context).size.width/3,
+                          height: MediaQuery.of(context).size.height/8,
+                          decoration: new BoxDecoration(
+                            color: Colors.white,
+                            border: new Border.all(color: Colors.white, width: 2.0),
+                            borderRadius: new BorderRadius.circular(10.0),
+                          ),
+                          child: new Center(child:
+                          new Text('Order Now',
+                            style: new TextStyle(
+                                fontSize: 18.0,
+                                color: Colors.red,
+                                fontWeight:
+                                FontWeight.w700),
+                          ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
               child: Image.asset(
-                  'shop-icon.png',
-                   width: 22,
-                   fit: BoxFit.cover,
+                'shop-icon.png',
+                width: 22,
+                fit: BoxFit.cover,
               ),
             ),
       ),
     ];
 
     return Scaffold(
-      body: Center(
-          child: Container(
-            child: Column(
-              children: [
-                Flexible(
-                  child: FlutterMap(options:
-                  MapOptions(
-                    //Whenever user clicks on the shops icon in the bottom nav bar. It will direct them to UPM Corrdinates
-                    center: LatLng(2.9993,101.7079),
-                    zoom: 14
-                  ),
-                    layers: [
-                      TileLayerOptions(
-                        urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                        subdomains: ['a', 'b', 'c'],
-                        attributionBuilder: (_) {
-                          return Text("© OpenStreetMap contributors");
-                        },
+        body: Center(
+            child: Container(
+                child: Column(
+                  children: [
+                    Flexible(
+                      child: FlutterMap(options:
+                      MapOptions(
+                        //Whenever user clicks on the shops icon in the bottom nav bar. It will direct them to UPM Corrdinates
+                          center: LatLng(2.9993,101.7079),
+                          zoom: 14
                       ),
-                      MarkerLayerOptions(
-                        markers: marker,
+                        layers: [
+                          TileLayerOptions(
+                            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                            subdomains: ['a', 'b', 'c'],
+                            attributionBuilder: (_) {
+                              return Text("© OpenStreetMap contributors");
+                            },
+                          ),
+                          MarkerLayerOptions(
+                            markers: marker,
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
+                    )
+                  ],
                 )
-              ],
-            )
-      ))
+            ))
     );
 
   }
